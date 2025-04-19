1e5856f4a55ee73885ee48f5a257fe8bce313475
guojw <2315162186@qq.com>
2025-04-19 17:16:59 +0800

    feat:  deploy

--- src/utils/const.ts
+++
@@ -1,3 +0,0 @@
-export const APP_ID = '61c7d9aef3c704b7f8b0fd8000fc1ac5'; // todo use dotenv read from .env file
-export const GEO_BASE_URL = 'https://api.openweathermap.org/geo/1.0/';
-export const BASE_URL = 'https://api.openweathermap.org/data/3.0/';
